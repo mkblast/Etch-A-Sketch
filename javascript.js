7d109950f4e78221e093248b95f1637dae5e1c58
Karim Mk <mk47blast@protonmail.com>
2022-03-16 20:19:10 +0100

    The main feature in this commit is adding the color pickr option. - Removing the gaps between the squares. - Style the color picker but nothing is final.

--- javascript.js
+++ javascript.js
@@ -14,7 +14,11 @@ function createElement(num) {
   // We call this function so when we create the new canvas
   // we will be able to color it.
   colorChild()
+  pickColor()
 }
+
+// the default canvas has 256 squars (16*16).
+createElement(256)
 
 // A function to delete the .element nodes
 // we mainly use this function when we change the size of the canvas.
@@ -24,22 +28,6 @@ function deleteChild() {
     element.remove()
   })
 }
-
-// The main function to color the squars, now is set to black.
-// more colors will be added later.
-function colorChild() {
-  const elements = document.querySelectorAll('.element')
-  elements.forEach((element) => {
-    element.addEventListener(('mouseover'), () => {
-      element.style.backgroundColor = 'black'
-    })
-  })
-}
-
-// the default canvas has 256 squars (16*16).
-createElement(256)
-
-
 
 // This is the event listener that will change the canvas
 // when we click on any button.
@@ -56,3 +44,23 @@ btns.forEach((btn) => {
 
   })
 })
+
+let defaulColor = '#000000'
+function pickColor() {
+  const picker = document.querySelector('.picker')
+  picker.addEventListener('change', () => {
+    defaulColor = picker.value
+  })
+}
+
+// The main function to color the squars, now is set to black.
+// more colors will be added later.
+function colorChild() {
+  const elements = document.querySelectorAll('.element')
+  elements.forEach((element) => {
+    element.addEventListener(('mouseover'), () => {
+      element.style.backgroundColor = defaulColor
+    })
+  })
+}
+
